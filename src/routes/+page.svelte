a50e5bb4d1d15700a10797183b0422ace5420971
Maximilian Kienitz <max@kienitz.dev>
2024-03-19 21:23:07 +0100

    feat: sort coffees by roasting date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,19 @@
 	import CoffeeCard from '$lib/components/coffee-card.svelte';
 	import { Button } from '$lib/components/ui/button';
 	export let data;
+	const coffees = data.coffees.toSorted((a, b) => {
+		if (a.roastingDate < b.roastingDate) {
+			return 1;
+		} else if (a.roastingDate > b.roastingDate) {
+			return -1;
+		}
+		return 0;
+	});
 </script>
 
-<div class="flex flex-col space-y-4">
-	<Button href="/coffees">Add new Coffee</Button>
-	{#each data.coffees as coffee}
+<div class="flex flex-col items-center space-y-4">
+	<Button href="/coffees" class="w-fit">Add new Coffee</Button>
+	{#each coffees as coffee}
 		<CoffeeCard {coffee} />
 	{/each}
 </div>
