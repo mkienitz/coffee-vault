65bbcda1b6b764412a0e881644caa1077b407176
Maximilian Kienitz <max@kienitz.dev>
2024-03-21 02:39:05 +0100

    feat: improve card styling

--- src/lib/components/coffee-card.svelte
+++ src/lib/components/coffee-card.svelte
@@ -5,23 +5,29 @@
 	import { getCountryCode, getEmojiFlag } from 'countries-list';
 	import type { z } from 'zod';
 	import type { coffeeSchema } from '$lib/schemas';
+	import { cn } from '$lib/utils';
+	import { Separator } from '$lib/components/ui/separator';
+	import { Badge } from '$lib/components/ui/badge';
 	export let coffee: z.infer<typeof coffeeSchema>;
 	const flag = getEmojiFlag(getCountryCode(coffee.country) || 'CO');
 	const dummyWeight = Math.floor(Math.random() * coffee.weight);
 </script>
 
-<Card.Root class="w-[420px]">
-	<Card.Header>
+<Card.Root class={cn('flex flex-col ', $$restProps.class)}>
+	<Card.Header class="pb-0">
 		<Card.Title class="flex flex-col space-y-1">
 			<div class="flex flex-row items-center justify-between">
 				<div class="text-2xl">{`${flag} ${coffee.name}`}</div>
-				<Button href={`/coffees/${coffee.id}`} variant="link" class="p-0">Edit</Button>
+				<Button href={`/coffees/${coffee.id}`} variant="link" class="h-fit w-fit p-0">Edit</Button>
 			</div>
-			<div class="text-sm text-muted-foreground">{coffee.roaster}</div>
+			<div class="flex flex-row items-center justify-between">
+				<small class="text-muted-foreground">{coffee.roaster}</small>
+				<small class="text-muted-foreground">{coffee.roastingDate}</small>
+			</div>
 		</Card.Title>
 	</Card.Header>
-	<Card.Content class="flex flex-col space-y-4">
-		<div></div>
+	<Separator class="my-3" />
+	<Card.Content class="flex grow flex-col space-y-4 pb-0">
 		<div class="flex flex-col space-y-1">
 			<div class="font-bold">Origin</div>
 			<div class="text-sm">{coffee.farm}, {coffee.region}, {coffee.country}</div>
@@ -35,10 +41,14 @@
 			<div class="text-sm">{coffee.process}</div>
 		</div>
 	</Card.Content>
-	<Card.Footer class="flex flex-row items-start justify-between">
-		<div class="text-muted-foreground">Roasted: {coffee.roastingDate}</div>
-		<div class="flex w-1/3 flex-col items-center space-y-1">
-			<Progress value={(dummyWeight / coffee.weight) * 100} />
+	<Separator class="my-3" />
+	<Card.Footer class="flex w-full flex-row justify-between justify-self-end">
+		<div>
+			<Badge variant="outline">some</Badge>
+			<Badge variant="outline">badges</Badge>
+		</div>
+		<div class="flex flex-row items-center space-x-1">
+			<Progress class="w-[80px]" value={(dummyWeight / coffee.weight) * 100} />
 			<div class="text-sm text-muted-foreground">{dummyWeight}/{coffee.weight}g</div>
 		</div>
 	</Card.Footer>
