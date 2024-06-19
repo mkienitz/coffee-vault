1de4fff03ed23f12e52e335fb292c589c3d992ce
Maximilian Kienitz <max@kienitz.dev>
2024-06-19 02:49:27 +0200

    fix: add guard against undefined keys in dev import function

--- src/routes/api/bq/import/+server.ts
+++ src/routes/api/bq/import/+server.ts
@@ -1,15 +1,16 @@
 import type { RequestHandler } from '@sveltejs/kit';
-import BQJson from './bq.json'
+import BQJson from './bq.json';
 import { coffees, db } from '$lib/db';
 import { coffeeSchema } from '$lib/schemas';
 
-type BQ = typeof BQJson
+type BQ = typeof BQJson;
 
 export const POST: RequestHandler = async ({ request }) => {
 	const bqData: BQ = await request.json();
 	const beans = bqData.BEANS;
 	for (const bean of beans) {
-		const { farm, farmer, variety, country, elevation, processing, region }: any = bean.bean_information[0];
+		const { farm, farmer, variety, country, elevation, processing, region }: any =
+			bean.bean_information[0];
 		const { weight, note, roastingDate, roaster, name, aromatics } = bean;
 		const coffee: any = {
 			country,
@@ -28,14 +29,14 @@ export const POST: RequestHandler = async ({ request }) => {
 		};
 		for (const key in coffee) {
 			if (coffee[key] === undefined) {
-				coffee[key] = ''
+				coffee[key] = '';
 			}
 		}
-		const res = await coffeeSchema.safeParseAsync(coffee)
+		const res = await coffeeSchema.safeParseAsync(coffee);
 		if (!res.success) {
-			console.error(coffee)
-			console.error(res.error)
-			continue
+			console.error(coffee);
+			console.error(res.error);
+			continue;
 		}
 		await db.insert(coffees).values(res.data);
 	}
