c4d3d0f2cf3f12838b9ce81c7225d6d9278d22d6
Maximilian Kienitz <max@kienitz.dev>
2024-06-20 23:28:37 +0200

    feat(doses): don't allow dose creation if there's not enough coffee

--- src/routes/coffees/[[coffeeId]]/doses/+page.server.ts
+++ src/routes/coffees/[[coffeeId]]/doses/+page.server.ts
@@ -25,27 +25,46 @@ export const load: PageServerLoad = async ({ params }) => {
 };
 
 export const actions: Actions = {
-	add: async ({ request, cookies }) => {
+	add: async ({ request, cookies, params }) => {
+		// Validate dose data
 		const formData = await request.formData();
-		console.log(formData);
 		const form = await superValidate(formData, zod(doseSchema));
 		if (!form.valid) {
 			return fail(400, {
 				form
 			});
 		}
+		// We don't want edit capability -> fresh IDs
 		if (form.data.id) {
 			setFlash({ type: 'success', message: 'Dose ID should not be set!' }, cookies);
 			return fail(400, {
 				form
 			});
 		}
+		// Make sure there is enough coffee left to dose
+		const coffee = await db.query.coffees.findFirst({
+			where: eq(coffees.id, Number(params.coffeeId)),
+			with: {
+				doses: true
+			}
+		});
+		if (!coffee) {
+			throw error(404, 'Coffe not found. Cannot show doses.');
+		}
+		const dosed = coffee.doses.map((dose) => dose.weight).reduce((a, b) => a + b, 0)
+		const undosed = coffee.weight - dosed
+		if (form.data.weight > undosed) {
+			setFlash({ type: 'error', message: 'Not enough coffee left to create new dose' }, cookies);
+			return fail(400, {
+				form
+			});
+		}
+
 		await db.insert(doses).values(form.data);
 		return { form };
 	},
 	delete: async ({ request }) => {
 		const formData = await request.formData();
-		console.log(formData);
 		const form = await superValidate(formData, zod(doseSchema));
 		if (!form.valid) {
 			return fail(400, {
