3e58178d269c8f82e39fd8d7ea9892a09a5a6a23
Maximilian Kienitz <max@kienitz.dev>
2024-06-22 07:02:58 +0200

    chore: prettier

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ export default defineConfig({
 	schema: './src/lib/db/index.ts',
 	dialect: 'sqlite',
 	dbCredentials: {
-		url: process.env.COFFEE_VAULT_DB_PATH!,
+		url: process.env.COFFEE_VAULT_DB_PATH!
 	},
 	out: './drizzle',
 	verbose: true,
