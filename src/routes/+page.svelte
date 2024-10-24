989c1ee540681a9af1a03cc1d5d97e77cde0e8d7
Maximilian Kienitz <max@kienitz.dev>
2024-10-24 20:15:40 +0200

    feat: make table view the landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,91 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import CoffeeCard from '$lib/components/coffee-card.svelte';
-	import { Button } from '$lib/components/ui/button';
+	import { getCountryCode, getEmojiFlag, type TCountryCode } from 'countries-list';
 	import * as Pagination from '$lib/components/ui/pagination';
-	import { MoveLeft, MoveRight } from 'lucide-svelte';
+	import * as Table from '$lib/components/ui/table';
+	import { ArrowDownZa, ArrowUpAz, MoveLeft, MoveRight } from 'lucide-svelte';
+	import { Badge } from '$lib/components/ui/badge';
+	import * as HoverCard from '$lib/components/ui/hover-card';
+	import type { CoffeeWithDoses } from '$lib/schemas';
+	import { Button } from '$lib/components/ui/button/index.js';
 
 	let { data } = $props();
+	const coffees = $state(data.coffees);
 
-	const coffees = $state(
-		data.coffees.toSorted((a, b) => {
-			if (a.roastingDate < b.roastingDate) {
-				return 1;
-			} else if (a.roastingDate > b.roastingDate) {
-				return -1;
+	// FILTERING
+	type Filter = (coffee: CoffeeWithDoses) => boolean;
+	let currentFilter: Filter = $state((_) => true);
+	let filteredCoffees = $derived(coffees.filter(currentFilter));
+
+	// SORTING
+	type SortingFunction = (a: CoffeeWithDoses, b: CoffeeWithDoses) => number;
+	let sortedBy = $state('roastingDate');
+	let ascending = $state(true);
+
+	const createSortingFunction: (fieldName: string) => SortingFunction = (fieldName) => {
+		return (c1, c2) => {
+			const accessor = (coffee: CoffeeWithDoses) => coffee[fieldName as keyof typeof coffee];
+			if (accessor(c1) === accessor(c2)) {
+				return 0;
 			}
-			return 0;
-		})
-	);
+			if (accessor(c1) < accessor(c2)) {
+				return ascending ? -1 : 1;
+			} else {
+				return ascending ? 1 : -1;
+			}
+		};
+	};
 
-	// Pagination
-	const pageSize = 8;
-	const nPages = Math.ceil(coffees.length / pageSize);
+	const sortBy = (fieldName: string) => {
+		if (sortedBy === fieldName) {
+			ascending = !ascending;
+		}
+		sortedBy = fieldName;
+	};
+
+	let currentSortingFunction: SortingFunction = $derived(createSortingFunction(sortedBy));
+	let sortedCoffees = $derived(filteredCoffees.toSorted(currentSortingFunction));
+
+	// PAGINATION
+	const pageSize = 15;
+	const nPages = $derived(Math.ceil(filteredCoffees.length / pageSize));
 	let currPage = $state(Math.min(data.page, nPages));
-	let currCoffees = $derived(
-		coffees.slice((currPage - 1) * pageSize, (currPage - 1) * pageSize + pageSize)
+	$effect(() => {
+		currPage = Math.min(data.page, nPages);
+	});
+
+	let paginatedCoffees = $derived(
+		sortedCoffees.slice((currPage - 1) * pageSize, (currPage - 1) * pageSize + pageSize)
 	);
 	$effect(() => {
 		goto(`?page=${currPage}`);
 	});
 </script>
 
+{#snippet tableHead(headerName: string, fieldName: string)}
+	<Table.Head>
+		<Button
+			variant="ghost"
+			onclick={() => {
+				sortBy(fieldName);
+			}}
+			>{headerName}
+			{#if sortedBy === fieldName}
+				{#if ascending}
+					<ArrowUpAz />
+				{:else}
+					<ArrowDownZa />
+				{/if}
+			{/if}
+		</Button>
+	</Table.Head>
+{/snippet}
+
 <div>
 	<div class="flex flex-col items-center space-y-4">
 		<div class="flex w-full flex-row justify-between">
 			<Pagination.Root
-				count={coffees.length}
+				count={filteredCoffees.length}
 				perPage={pageSize}
 				bind:page={currPage}
 				let:pages
@@ -67,12 +118,73 @@
 				</Pagination.Content>
 			</Pagination.Root>
 		</div>
-		<Button href="/coffees">Add a new Coffee</Button>
-		<div class="grid grid-cols-1 gap-4 md:grid-cols-2">
-			<!-- https://stackoverflow.com/questions/71883001/svelte-state-of-components-rendered-with-each-loop -->
-			{#each currCoffees as coffee (coffee.id)}
-				<CoffeeCard {coffee} />
-			{/each}
+		<div class="flex w-full flex-row justify-between">
+			<Button
+				variant="secondary"
+				onclick={() => {
+					currentFilter = (_) => true;
+				}}>Reset Filters</Button
+			>
+			<Button href="/coffees">Add Coffee</Button>
 		</div>
+		<Table.Root>
+			<Table.Header>
+				<Table.Row>
+					{@render tableHead('Name', 'name')}
+					{@render tableHead('Country', 'country')}
+					{@render tableHead('Variety', 'varietals')}
+					{@render tableHead('Roaster', 'roaster')}
+					{@render tableHead('Process', 'process')}
+					{@render tableHead('Roast Date', 'roastingDate')}
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+				{#each paginatedCoffees as coffee (coffee.id)}
+					<Table.Row class="content-center">
+						<Table.Cell
+							><Button variant="link" href="/coffees/{coffee.id}/doses">{coffee.name}</Button
+							></Table.Cell
+						>
+						<Table.Cell>
+							<Button
+								variant="ghost"
+								onclick={() => {
+									currentFilter = (c) => c.country === coffee.country;
+								}}
+							>
+								{`${getEmojiFlag(getCountryCode(coffee.country) as TCountryCode)} ${coffee.country}`}
+							</Button>
+						</Table.Cell>
+						<Table.Cell
+							><Button
+								variant="ghost"
+								onclick={() => {
+									currentFilter = (c) => c.varietals === coffee.varietals;
+								}}>{coffee.varietals}</Button
+							></Table.Cell
+						>
+						<Table.Cell
+							><Button
+								variant="ghost"
+								onclick={() => {
+									currentFilter = (c) => c.roaster === coffee.roaster;
+								}}>{coffee.roaster}</Button
+							></Table.Cell
+						>
+						<Table.Cell class="text-center">
+							<HoverCard.Root>
+								<HoverCard.Trigger target="_blank">
+									<Badge variant="outline" class="bg-pink-600">washed</Badge>
+								</HoverCard.Trigger>
+								<HoverCard.Content class="w-fit text-center"
+									><span class="text-sm">{coffee.process}</span></HoverCard.Content
+								>
+							</HoverCard.Root>
+						</Table.Cell>
+						<Table.Cell class="text-center">{coffee.roastingDate}</Table.Cell>
+					</Table.Row>
+				{/each}
+			</Table.Body>
+		</Table.Root>
 	</div>
 </div>
