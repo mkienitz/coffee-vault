8314946c626607c84a7ba1b13f3dcf36f4e59cf9
Maximilian Kienitz <max@kienitz.dev>
2025-03-15 03:51:16 +0100

    feat: misc layout changes, including a navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
-<script>
+<script lang="ts">
 	import { getFlash } from 'sveltekit-flash-message';
 	import { toast, Toaster } from 'svelte-french-toast';
 	import { page } from '$app/state';
+	import { Menu } from 'lucide-svelte';
 
 	const { children } = $props();
 
@@ -22,14 +23,41 @@
 </script>
 
 <Toaster />
+
+{#snippet NavElem(pathname: string)}
+	{@const path = `/${pathname.toLowerCase()}`}
+	<li><a href={path} class={path === page.url.pathname ? 'underline font-bold' : ''}>{pathname}</a></li>
+{/snippet}
+
+{#snippet NavMenu()}
+	{@render NavElem('Coffees')}
+	{@render NavElem('Doses')}
+	{@render NavElem('Brews')}
+	{@render NavElem('Mystery')}
+{/snippet}
+
 <header>
-	<nav class="flex items-end justify-between pt-4 pb-8">
-		<a href="/" class="text-secondary-foreground text-5xl">CoffeeVault</a>
-		<div class="space-x-4">
-			<a href="/"><span class="text-xl">Coffees</span></a>
-			<a href="/doses"><span class="text-xl">Doses</span></a>
+	<div class="navbar mb-6 shadow-sm">
+		<div class="navbar-start">
+			<div class="dropdown">
+				<div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+					<Menu />
+				</div>
+				<ul class="menu dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+					{@render NavMenu()}
+				</ul>
+			</div>
+			<a href="/" class="btn btn-ghost text-xl no-underline">CoffeeVault</a>
 		</div>
-	</nav>
+		<div class="navbar-center hidden lg:flex">
+			<ul class="menu menu-horizontal px-1">
+				{@render NavMenu()}
+			</ul>
+		</div>
+		<div class="navbar-end">
+			<a href="/login" class="btn">Login</a>
+		</div>
+	</div>
 </header>
 <main class="flex flex-col items-center">
 	{@render children()}
