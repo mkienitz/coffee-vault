61f50223c9d39388f997fbcec1e452254497dae3
Maximilian Kienitz <max@kienitz.dev>
2025-03-16 03:57:40 +0100

    feat(components/CoffeeCard): make weight status bar dynamic in color, fix text shades

--- src/lib/components/CoffeeCard.svelte
+++ src/lib/components/CoffeeCard.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
 	import { type Coffee, type Dose, type Brew } from '$lib/zod-schemas';
-	import { getCoffeeFlag } from '$lib/utils'
+	import { getCoffeeFlag } from '$lib/utils';
 
 	const { coffee, doses, brews }: { coffee: Coffee; doses: Dose[]; brews: Brew[] } = $props();
 
 	const remainingWeight = $derived(
 		coffee.weight - brews.reduce((acc, brew) => acc + brew.weight, 0)
 	);
+
+	const progressBarColor = $derived.by(() => {
+		if (remainingWeight < 7) {
+			return 'text-red-500';
+		} else if (remainingWeight < 20) {
+			return 'text-orange-500';
+		} else if (remainingWeight < 0.3 * coffee.weight) {
+			return 'text-yellow-500';
+		} else {
+			return 'text-green-500';
+		}
+	});
 </script>
 
 <div class="card flex max-h-fit w-[400px] flex-col shadow-xl">
@@ -19,51 +31,49 @@
 				<a href="/coffees/{coffee.id}/edit" class="link h-fit w-fit p-0">Edit</a>
 			</div>
 			<div class="flex w-[100%] flex-row items-center justify-between">
-				<small class="text-muted-foreground">{coffee.roaster}</small>
-				<small class="text-muted-foreground">{coffee.roastingDate}</small>
+				<small class="text-base-content/70">{coffee.roaster}</small>
+				<small class="text-base-content/70">{coffee.roastingDate}</small>
 			</div>
 		</div>
 		<div class="flex grow flex-col space-y-4 py-0">
 			<div class="flex flex-col space-y-1">
 				<div class="font-bold">Origin</div>
-				<div class="text-muted-foreground text-sm">
+				<div class="text-base-content/70 text-sm">
 					{[coffee.farm, coffee.region, coffee.country].filter((v) => v !== '').join(', ')}
 				</div>
 			</div>
 			{#if coffee.producer !== ''}
 				<div class="flex flex-col space-y-1">
 					<div class="font-bold">Producer</div>
-					<div class="text-muted-foreground text-sm">{coffee.producer}</div>
+					<div class="text-base-content/70 text-sm">{coffee.producer}</div>
 				</div>
 			{/if}
 			<div class="flex flex-col space-y-1">
 				<div class="font-bold">Varietals</div>
-				<div class="text-muted-foreground text-sm">{coffee.varietals}</div>
+				<div class="text-base-content/70 text-sm">{coffee.varietals}</div>
 			</div>
 			<div class="flex flex-col space-y-1">
 				<div class="font-bold">Process</div>
-				<div class="text-muted-foreground text-sm">{coffee.process}</div>
+				<div class="text-base-content/70 text-sm">{coffee.process}</div>
 			</div>
 			{#if coffee.notes !== ''}
 				<div class="flex flex-col space-y-1">
 					<div class="font-bold">Notes</div>
-					<div class="text-muted-foreground text-sm">{coffee.notes}</div>
+					<div class="text-base-content/70 text-sm">{coffee.notes}</div>
 				</div>
 			{/if}
 			<div class="flex flex-col space-y-1">
 				<div class="font-bold">Flavor Profile</div>
-				<div class="text-muted-foreground text-sm">{coffee.flavorProfile}</div>
+				<div class="text-base-content/70 text-sm">{coffee.flavorProfile}</div>
 			</div>
 		</div>
-		<div class="flex w-full flex-row justify-between">
-			<div>
-				<div class="badge badge-accent">some</div>
-				<div class="badge badge-primary">badges</div>
-			</div>
-			<div class="flex flex-row items-center space-x-2">
-				<progress class="progress w-[80px]" value={remainingWeight} max={coffee.weight}></progress>
-				<div class="text-muted-foreground text-sm">{remainingWeight}/{coffee.weight}g</div>
-			</div>
+		<div class="flex flex-row items-center space-x-2 self-end">
+			<progress
+				class="progress w-[160px] {progressBarColor}"
+				value={remainingWeight}
+				max={coffee.weight}
+			></progress>
+			<div class="text-base-content/70 text-sm">{remainingWeight}/{coffee.weight}g</div>
 		</div>
 	</div>
 </div>
