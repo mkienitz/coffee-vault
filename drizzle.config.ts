c1d76eb76743f165fb1a5798a2a588b38e5722a4
Maximilian Kienitz <max@kienitz.dev>
2025-06-07 02:29:23 +0200

    fix(drizzle): update path to schema definitions

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
 	out: './drizzle',
-	schema: './src/lib/db/schema.ts',
+	schema: './src/lib/server/db/schema.ts',
 	dialect: 'sqlite',
 	dbCredentials: {
 		url: 'db.sqlite'
